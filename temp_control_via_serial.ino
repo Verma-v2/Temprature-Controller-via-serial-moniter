20b6d5efd03b0d356bc27a0974b0041502da65be
Gyanendra Verma <gveeiit@gmail.com>
2019-02-28 00:04:30 +0530

    Update temp_control_via_serial.ino

--- temp_control_via_serial.ino
+++ temp_control_via_serial.ino
@@ -1,7 +1,12 @@
+/*   Copyright [2019] [Gyanendra Verma]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.*/
+
 #include<LiquidCrystal.h>
-#include <DallasTemperature.h>
+#include <DallasTemperature.h> //Library
 
-
+//Pins initilization
 int ONE_WIRE_BUS=2;
 
 const int rs = 12, en = 11, d4 = A3, d5 = A2, d6 = A1, d7 = A0;
@@ -9,13 +14,17 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int powled= 8;
 int led= 3;
 int ledPin = 13;
-
+// variable Setup
 float dstemp; 
-int mintemp = 23;
-int maxtemp = 29;
+int mintemp = 23; // default Min Temperature
+int maxtemp = 29; // default Min Temperature
+
 
 OneWire oneWire(ONE_WIRE_BUS); 
 DallasTemperature sensors(&oneWire);
+
+
+/****************************************************void setup******************************************************************************/
 void setup() {
    Serial.begin(9600);
      lcd.setCursor(0,0);
@@ -29,7 +38,7 @@ pinMode(powled,OUTPUT);
  pinMode(ledPin, OUTPUT); 
   
 }
-
+/**********************************************************************void loop***********************************/
 void loop() {
   sensors.requestTemperatures(); 
  dstemp= (sensors.getTempCByIndex(0));
@@ -46,7 +55,7 @@ void loop() {
 //  lcd1.print(0, 0, "Temp= ");
   //lcd1.print( 5,0, dstemp );
 
-
+/*************************************************************Logic**********************************************************/
  if ( dstemp >=mintemp && dstemp<= maxtemp){
       digitalWrite(powled,HIGH);
       digitalWrite(led,LOW);
@@ -69,7 +78,7 @@ void loop() {
         
        
         }
-
+/******************************************Serial Read*********************************************************************/
         if(Serial.available())
   {
     char temp = Serial.read();
